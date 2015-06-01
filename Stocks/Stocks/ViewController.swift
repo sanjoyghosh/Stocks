ea8eccaea4c92366274ef04dc403aa1c9fb77064
Sanjoy Ghosh <sghosh@hytrust.com>
2015-06-01 06:26:48 -0700

    Stocks

---
+++ Stocks/Stocks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Sanjoy Ghosh on 5/31/15.
+//  Copyright (c) 2015 Sanjoy Ghosh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
